92f5034f0d1d088662633db21dc15ef2a4232605
Ramazan Mert Kaya <kmert0@outlook.com>
2023-04-04 17:05:58 +0300

    passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem () {
-    return (
-<div className='expense-item'>
-    <div>March 3th 2023</div>
-    <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294</div>
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-</div>
-    );
+  );
 }
 
 export default ExpenseItem;
